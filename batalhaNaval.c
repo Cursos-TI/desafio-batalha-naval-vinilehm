438cd2bff057e25815705c973d2e16819aec7552
vinilehm <vinilehmkuhlfreitas@gmail.com>
2025-05-07 19:48:34 +0000

    NIvel Aventureiro!

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -27,6 +27,8 @@ for(i = 0; i < tamanhoTabuleiro; i++) {
 
 int NavioHorizontal[3] = {valorNavio, valorNavio, valorNavio};
 int NavioVertical[3] = {valorNavio, valorNavio, valorNavio};
+int NavioHorizontal1[3] = {valorNavio, valorNavio, valorNavio};
+int NavioHorizontal2[3] = {valorNavio, valorNavio, valorNavio};
 
 int linhaHorizontal = 1;//Y
 int colunaHorizontal = 2;//x
@@ -34,10 +36,21 @@ int colunaHorizontal = 2;//x
 int linhaVertical = 5;//Y
 int colunaVertical = 7;//X
 
+int linhaHorizontal1 = 4;//Y
+int colunaHorizontal1 = 4;//X
+
+int linhaHorizontal2 = 6;//Y
+int colunaHorizontal2 = 2;//X
+
+
 
 
 if(colunaHorizontal + valorNavio <= tamanhoTabuleiro &&
-    linhaVertical + valorNavio <= tamanhoTabuleiro) {
+    linhaVertical + valorNavio <= tamanhoTabuleiro &&
+    linhaHorizontal1 + valorNavio <= tamanhoTabuleiro &&
+    colunaHorizontal1 + valorNavio <= tamanhoTabuleiro &&
+    linhaHorizontal2 + valorNavio <= tamanhoTabuleiro &&
+    colunaHorizontal2 + valorNavio <= tamanhoTabuleiro) {
 
         for(i = 0; i < valorNavio; i++){
         tabuleiro[linhaHorizontal + i][colunaHorizontal] = NavioHorizontal[i];
@@ -46,6 +59,17 @@ if(colunaHorizontal + valorNavio <= tamanhoTabuleiro &&
         for(i = 0; i < valorNavio; i++) {
         tabuleiro[linhaVertical][colunaVertical + i] = NavioVertical[i];//declara parte do navio de acordo com a linha que é 10x10 e a coluna 10x10
         }
+
+        for(i = 0; i < valorNavio; i++) {
+            tabuleiro[linhaHorizontal1 - i][colunaHorizontal1 + i] = NavioHorizontal1[i];//declara parte do navio de acordo com a linha que é 10x10 e a coluna 10x10
+            }
+
+            for(i = 0; i < valorNavio; i++) {
+                tabuleiro[linhaHorizontal2 + i][colunaHorizontal2 + i] = NavioHorizontal2[i];//declara parte do navio de acordo com a linha que é 10x10 e a coluna 10x10
+                }
+    
+
+
 
 
         printf("Tabuleiro\n");
@@ -69,6 +93,6 @@ if(colunaHorizontal + valorNavio <= tamanhoTabuleiro &&
 
  
 
-    return 0; 
+    return 0; // Retorna 0 para indicar que o programa terminou com sucesso
 
 }
